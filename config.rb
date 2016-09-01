754c04426ffabeb40be4816268f0bd2769f5aa73
Antonija <ipekica@gmail.com>
2016-09-01 20:19:25 +0200

    saving code

--- config.rb
+++ config.rb
@@ -61,13 +61,30 @@ configure :build do
   activate :minify_javascript
 end
 
+# activate :deploy do |deploy|
+#   deploy.deploy_method = :ftp
+#   deploy.host          = 'admin@udrugarea.hr'
+#   deploy.path          = ''
+#   # Optional Settings
+#   deploy.user  = 'admin' # no default
+#   # deploy.port  = 21098  # ssh port, default: 22
+#   deploy.clean = true # remove orphaned files on remote host, default: false
+#   # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
+# end
+
 activate :deploy do |deploy|
-  deploy.deploy_method = :rsync
-  deploy.host          = 'antonijapek.com'
-  deploy.path          = '/home/antonija/public_html/dev'
+  deploy.deploy_method = :git
   # Optional Settings
-  deploy.user  = 'antonija' # no default
-  deploy.port  = 21098  # ssh port, default: 22
-  deploy.clean = true # remove orphaned files on remote host, default: false
-  # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
+  deploy.remote   = 'git@github.com:antonijap/udrugarea.git' # remote name or git url, default: origin
+  deploy.branch   = 'master' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+# activate :deploy do |deploy|
+#   deploy.deploy_method   = :ftp
+#   deploy.host            = 'ftp.udrugarea.hr'
+#   deploy.path            = '/home/antonija/public_html'
+#   deploy.user            = 'admin'
+#   deploy.password        = 'reajezakon1'
+# end
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,3 @@
 $(function(){
 	$('#udomi').mixItUp();
-	
-	// for custom error message on invalid field
-	$(document).bind('invalid.zf.abide',function(e) {
-	  console.log("Sorry, "+e.target.id+" is not valid");
-	});
 });
